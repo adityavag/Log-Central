d7f7b7c7116ec72c003c21b652ce8424ff518cf4
adityavag <adi247u@gmail.com>
2024-07-14 15:33:50 +0530

    Ingestor Form Doone

--- client/src/pages/Ingestor.jsx
+++ client/src/pages/Ingestor.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
+import axios from 'axios';
 import ingest from '../assets/images/ingest.jpg';
+
 export default function Ingestor() {
 
   const [formData, setFormData] = useState({
@@ -13,117 +15,139 @@ export default function Ingestor() {
     metadata: ''
   });
 
+  const insertCurrentTime = () => {
+    const currentTime = new Date().toISOString;
+    setFormData((prevData) => ({ ...prevData, timestamp: currentTime }));
+  }
   const handleInputChange = (e) => {
+    console.log(e.target.name + " : " + e.target.value)
     setFormData((prevData) => ({
       ...prevData, [e.target.name]: [e.target.value]
     }));
   }
   const handleSubmit = (e) => {
-    setFormData(
-      {
-        level: '',
-        message: '',
-        resourceId: '',
-        timestamp: '',
-        traceId: '',
-        spanId: '',
-        commit: '',
-        metadata: ''
-      });
-    console.log('Ingested')
+    e.preventDefault();
+    console.log(formData)
+    try {
+      console.log(formData);
+      setFormData(
+        {
+          level: '',
+          message: '',
+          resourceId: '',
+          timestamp: '',
+          traceId: '',
+          spanId: '',
+          commit: '',
+          metadata: ''
+        });
+      console.log('Ingested')
+    } catch (error) {
+      console.log('Error Ingesting Log :', error);
+    }
   }
 
   return (
-    <div className='p-12 grid grid-cols-2 max-md:grid-cols-1 max-md:space-y-5 max-sm:grid-cols-1'>
+    <div className='p-12 grid grid-cols-2 max-md:grid-cols-1 max-md:space-y-5 max-sm:grid-cols-1 max-lg:grid-cols-1'>
       {/* Form */}
       <div className='flex flex-col justify-center'>
-        <form>
-        <span className='text-2xl font-semibold'>Ingest New Log</span>
+        <form className='' onSubmit={handleSubmit}>
+          <span className='text-2xl font-semibold'>Ingest New Log</span>
           <div className='' id='form'>
             {/* Label */}
             <div>
-              <label className='block mb-1 font-bold text-gray-700'>Level</label>
+              <label className='block mb-1 font-bold text-gray-700' htmlFor='level'>Level</label>
               <input className='w-full p-1 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline'
                 id="level"
                 type='text'
+                name='level'
                 value={formData.level}
                 onChange={handleInputChange}
-                name='level' />
+              />
             </div>
             {/* Message */}
             <div>
-              <label className='block mb-1 font-bold text-gray-700'>Message</label>
+              <label className='block mb-1 font-bold text-gray-700' htmlFor='message'>Message</label>
               <input className='w-full p-1 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline'
                 id="message"
                 type='text'
+                name='message'
                 value={formData.message}
                 onChange={handleInputChange}
-                name='message' />
+              />
             </div>
             {/* Resource ID */}
             <div>
-              <label className='block mb-1 font-bold text-gray-700'>Resource ID</label>
+              <label className='block mb-1 font-bold text-gray-700' htmlFor='resourceId'>Resource ID</label>
               <input className='w-full p-1 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline'
                 id="resourceId"
                 type='text'
+                name='resourceId'
                 value={formData.resourceId}
                 onChange={handleInputChange}
-                name='resourceId' />
+              />
             </div>
             {/* TimeStamp */}
             <div>
-              <label className='block mb-1 font-bold text-gray-700'>TimeStamp</label>
+              <label className='block mb-1 font-bold text-gray-700' htmlFor='timestamp'>TimeStamp</label>
               <input className='w-full p-1 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline'
                 id="timestamp"
                 type='text'
+                name='timestamp'
                 value={formData.timestamp}
                 onChange={handleInputChange}
-                name='timestamp' />
+              />
             </div>
             {/* Trace ID */}
             <div>
-              <label className='block mb-1 font-bold text-gray-700'>Trace ID</label>
+              <label className='block mb-1 font-bold text-gray-700' htmlFor='traceId'>Trace ID</label>
               <input className='w-full p-1 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline'
                 id="traceId"
                 type='text'
+                name='traceId'
                 value={formData.traceId}
                 onChange={handleInputChange}
-                name='traceId' />
+              />
             </div>
             {/* Spand ID */}
             <div>
-              <label className='block mb-1 font-bold text-gray-700'>Span ID</label>
+              <label className='block mb-1 font-bold text-gray-700' htmlFor='spanId'>Span ID</label>
               <input className='w-full p-1 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline'
                 id="spanId"
                 type='text'
+                name='spanId'
                 value={formData.spanId}
                 onChange={handleInputChange}
-                name='spanId' />
+              />
             </div>
             {/* Commit */}
             <div>
-              <label className='block mb-1 font-bold text-gray-700'>Commit</label>
+              <label className='block mb-1 font-bold text-gray-700' htmlFor='commit'>Commit</label>
               <input className='w-full p-1 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline'
                 id="commit"
                 type='text'
+                name='commit'
                 value={formData.commit}
                 onChange={handleInputChange}
-                name='commit' />
+
+              />
             </div>
             {/* Metadata */}
             <div>
-              <label className='block mb-1 font-bold text-gray-700'>MetaData</label>
+              <label className='block mb-1 font-bold text-gray-700' htmlFor='metadata'>MetaData</label>
               <input className='w-full p-1 leading-tight text-gray-700 border rounded focus:outline-none focus:shadow-outline'
                 id="metadata"
                 type='text'
+                name='metadata'
                 value={formData.metadata}
                 onChange={handleInputChange}
-                name='metadata' />
+              />
             </div>
           </div>
           <button
-          type='submit'
-          className='bg-[#e297de] p-2 mt-2 w-full rounded-md hover:bg-[#854081] hover:text-white font-semibold'>
+            type='submit'
+            className='bg-[#e297de] p-2 mt-2 w-full rounded-md hover:bg-[#854081] hover:text-white font-semibold'
+          >
             Ingest
           </button>
         </form>
